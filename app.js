fb19e8dfc68cddd12c9de716b0d5911e94770f29
Magnus Peters Munoz <magnuspetersonmunoz@gmail.com>
2020-04-25 10:10:52 -0600

    Remove .vs folder

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
+//Dependencies
 var express = require("express")
 mongoose = require("mongoose")
-var app = express()
+app = express()
+
+//MongoDB setup
+mongoose.connect('mongodb://localhost:27017/work_app', { useNewUrlParser: true })
+mongoose.set('useUnifiedTopology', true);
+
+//Schema
+var emailSchema = new mongoose.Schema({
+    email: String,
+})
+var email =  mongoose.model("Email", emailSchema)
+
+
+//Content
 app.set('view engine','ejs')
 
 app.get('/', function(req, res){
-    res.render('test')
+    res.render('landing')
 })
 
+//Server call
 app.listen(4005, function(){
     console.log('RUNNING ON PORT 4005')
 })
