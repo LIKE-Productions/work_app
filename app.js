7a7eabf9cebee9e073d772013195222315e92e16
Magnus Peters Munoz <magnuspetersonmunoz@gmail.com>
2020-05-29 15:09:43 -0600

    add mammoth

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // Requirements
 var express = require('express'),
-    ejs = require('ejs')
+    ejs = require('ejs'),
+    mammoth = require('mammoth')
 var app = express();
 app.set('view engine', 'ejs');
+
+
+// Mammoth
+
+mammoth.convertToHtml({path: "../../../../Downloads/Federal Gov't - Debate Sheet.docx"})
+    .then(function(result){
+        var html = result.value; // The generated HTML
+        var messages = result.messages; // Any messages, such as warnings during conversion
+    })
+    .done();
 
 app.get('/', function (req, res) {
     res.render('landing');
